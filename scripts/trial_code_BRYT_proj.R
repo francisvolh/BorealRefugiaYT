d557f048df3b8cf71902857ba30a779ffcac9bb7
Van Oordt La Hoz <Francis.VanOordtLaHoz@ec.gc.ca>
2023-10-05 20:03:48 -0400

    fixing and adding some plots

--- scripts/trial_code_BRYT_proj.R
+++ scripts/trial_code_BRYT_proj.R
@@ -1,57 +1,438 @@
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(trelliscopejs)
 
+
+#############################################################
 # check the species output files names / list of birds
-setwd("C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/YT Boreal Refugia Drive/Plots_1991_Normal_Present Distributions")
+############################################################
 
-list.files()
 
-bird.names<-list.files()
+#########
+#SDM plots: Plots_1991_Normal_Present Distributions????
+list.files("data/YT Boreal Refugia Drive/Plots_1991_Normal_Present Distributions")
+
+bird.names<-list.files("data/YT Boreal Refugia Drive/Plots_1991_Normal_Present Distributions")
 
 bird.names1 <- substring(bird.names, 1,4)
+length(bird.names1)
 
 df.names1  <- as.data.frame(x=bird.names1)
-write.csv(df.names1, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/df.names1.csv")
+names(df.names1) <- "x"
+
+#write.csv(df.names1, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/df.names1.csv")
+#51 species
 
 
-setwd("C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/YT Boreal Refugia Drive/Files_1991_Present_Mean90CI_rds")
-list.files()
+#########
+#Files_1991_Present_Mean90CI_rds
+list.files("data/YT Boreal Refugia Drive/Files_1991_Present_Mean90CI_rds")
 
-bird.names<-list.files()
+bird.names<-list.files("data/YT Boreal Refugia Drive/Files_1991_Present_Mean90CI_rds")
 
 bird.names2 <- substring(bird.names, 1,4)
+length(bird.names2)
 
 df.names2  <- as.data.frame(x=bird.names2)
 
-write.csv(df.names2, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/df.names2.csv")
+#write.csv(df.names2, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/df.names2.csv")
+#53 species
 
 print(setdiff(bird.names2,bird.names1))
 
+
+#Files_1991_Future_Mean90CI_rd
+list.files("data/YT Boreal Refugia Drive/Files_1991_Future_Mean90CI_rds")
+#folder contains 3 scenarios and 3 climate models, then 9 files per species
+
+bird.names<-list.files("data/YT Boreal Refugia Drive/Files_1991_Future_Mean90CI_rds")
+head(bird.names, n=12)
+
+bird.names3 <- substring(bird.names, 1,4)
+length(bird.names3)
+length(bird.names3)/9
+
+df3<-data.frame(codes = bird.names3, file_name = bird.names)
+
+sum.df3 <- df3 %>%
+  group_by(codes) %>% 
+  summarise(
+    File.Count = n()
+  )
+
+#View(sum.df3)
+
+#pufi has 10 files
+df3 %>%
+  filter(codes ==  "PUFI")
+
+
+PUFIECEarth3__26_2071.1<-readRDS("data/YT Boreal Refugia Drive/Files_1991_Future_Mean90CI_rds/PUFIECEarth3__26_2071(1).rds")
+
+PUFIECEarth3__26_2071 <-readRDS("data/YT Boreal Refugia Drive/Files_1991_Future_Mean90CI_rds/PUFIECEarth3__26_2071.rds")
+
+summary(PUFIECEarth3__26_2071.1)
+summary(PUFIECEarth3__26_2071)
+
+#confirmed they are copied RDS files, deleting extra (1)
+
+bird.names3_cl <- unique(bird.names3)
+length(bird.names3_cl)
+bird.names3_cl
+df.names3  <- as.data.frame(x=bird.names3_cl)
+
+# 54 species listed, after cleaning up
+
+
+print(setdiff(bird.names3_cl,bird.names1))
+
+print(setdiff(bird.names3_cl,bird.names2))
+
+
+#######
+#Plots_1991_Normal_Refugia/Mean Plots
+
+list.files("data/YT Boreal Refugia Drive/Plots_1991_Normal_Refugia/Mean Plots")
+
+#folder contains 3 subfolders, using means folder
+
+bird.names<-list.files("data/YT Boreal Refugia Drive/Plots_1991_Normal_Refugia/Mean Plots")
+
+head(bird.names, n=12)
+
+bird.names4 <- substring(bird.names, 1,4)
+length(bird.names4)
+df.names4  <- as.data.frame(x=bird.names4)
+
+# 52 spp
+
+
+print(setdiff(bird.names4,bird.names1))
+
+print(setdiff(bird.names3,bird.names4))
+
+print(setdiff(bird.names2, bird.names4))
+
+
+#######
+
+###Rasters_1991_Normal_Refugia and Habitat Suitability ----- discrepancy some duplicate files
+#MEAN
+
+list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability MEAN")
+bird.names <- list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability MEAN")
+length(bird.names)
+length(bird.names)/3
+
+
+bird.names5 <- substring(bird.names, 1,4)
+
+df5<-data.frame(codes = bird.names5, file_name = bird.names)
+
+sum.df5 <- df5 %>%
+  group_by(codes) %>% 
+  summarise(
+    File.Count = n()
+  )
+
+View(sum.df5)
+
+#found a PDF named RegionMAT (a histogram overlap graph) moved into extra folder
+
+bird.names5 <- unique(bird.names5)
+
+df.names5  <- as.data.frame(x=bird.names5)
+
+#51 sp in mean folder
+
+print(setdiff(bird.names5,bird.names1))
+print(setdiff(bird.names2, bird.names5))
+print(setdiff(bird.names3, bird.names5))
+print(setdiff(bird.names4, bird.names5))
+
+print(setdiff(bird.names1, bird.names5))
+print(setdiff(bird.names5, bird.names2))
+print(setdiff(bird.names5, bird.names3))
+print(setdiff(bird.names5, bird.names4))
+
+
+#######
+###Rasters_1991_Normal_Refugia and Habitat Suitability ----- discrepancy some duplicate files
+
+#UPR90
+list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability UPR90")
+bird.names <- list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability UPR90")
+length(bird.names)
+length(bird.names)/3
+bird.names6 <- substring(bird.names, 1,4)
+
+bird.names6_cl <- unique(bird.names6)
+length(bird.names6_cl)
+
+df.bird.names6 <- data.frame(x=bird.names6_cl)
+
+#LWR90
+list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability LWR90")
+bird.names <- list.files("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability LWR90")
+length(bird.names)
+length(bird.names)/3
+bird.names7 <- substring(bird.names, 1,4)
+
+bird.names7 <- unique(bird.names7)
+length(bird.names7_cl)
+#52 spp but a bunch of repeated files
+
+idx<-grep("(1)", bird.names)
+
+to_delete <- bird.names[idx]
+
+file_name <- "data_file.txt"
+
+
+for (i in 1:length(to_delete)) {
+  if (file.exists(paste0("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability LWR90/",to_delete[i]))) {
+    #unlink(file_name)
+    print("File exists")
+  } else{
+    print("File not exists..")
+  } 
+}
+
+for (i in 1:length(to_delete)) {
+  if (file.exists(paste0("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability LWR90/",to_delete[i]))) {
+    unlink((paste0("data/YT Boreal Refugia Drive/Rasters_1991_Normal_Refugia and Habitat Suitability/1991 Refugia Habitat Suitability LWR90/",to_delete[i])))
+    print(paste0("File  ", i, "was deleted"))
+  } else{
+    print("File not exists..")
+  } 
+}
+
+
+df.bird.names7 <- data.frame(x=bird.names7_cl)
+
+###
+###BRT_output/GBMs_NoBarrier1991 ----- 152 files in folder
+#data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1991
+
+
+list.files("data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1991")
+
+bird.names <- list.files("data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1991")
+
+length(bird.names)
+length(bird.names)/2
+
+
+bird.names8 <- substring(bird.names, 1,4)
+
+bird.names8 <- unique(bird.names8)
+
+df.names8  <- as.data.frame(x=bird.names8)
+
+#76 sp in BRT 1991 normals
+
+
+###
+###BRT_output/GBMs_NoBarrier1991 ----- 106files in folder
+#data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1961
+
+
+list.files("data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1961")
+
+bird.names <- list.files("data/YT Boreal Refugia Drive/YK Refugia Code and material/BRT_output/GBMs_NoBarrier1961")
+
+length(bird.names)
+length(bird.names)/2
+
+
+bird.names9 <- substring(bird.names, 1,4)
+
+bird.names9 <- unique(bird.names9)
+
+df.names9  <- as.data.frame(x=bird.names9)
+
+#53 sp in BRT 1961 normals
+
+##################################################################
+
+
 #get alpha codes  already extrated from AOS file
 alpha.names <- read.csv(file.choose())
-
+class(alpha.names)
 alpha.names$X4.LETTER.CODE <-substring(alpha.names$X4.LETTER.CODE, 1,4)
 
 summary(alpha.names)
 
-names.full <-merge(df.names2, alpha.names, by.x="bird.names2", by.y= "X4.LETTER.CODE", all.x=TRUE)
-write.csv(names.full, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/names.full53.csv")
+#names.full <-merge(df.names2, alpha.names, by.x="1991_Present_Mean", by.y= "X4.LETTER.CODE", all.x=TRUE)
 
+#write.csv(names.full, "C:/Users/vanoordtlahozf/OneDrive - EC-EC/Documents/Boreal Refugia/names.full53.csv")
 
+#############################################################
 ### looking into the cleaned bird data
 
-all.birds <- read.csv(file.choose()) # AllBirdDataCleanedJul20.csv
+all.birds <- read.csv("data/YT Boreal Refugia Drive/YK Refugia Code and material/AllBirdDataCleanedJul20.csv") # 
+
+all.birds.spp <- unique(all.birds$spp)
+
+df.all.birds.spp <- data.frame(all.birds =  all.birds.spp)
+names(df.all.birds.spp)
+names(alpha.names)
+
+df.all.birds.spp <- df.all.birds.spp %>% 
+  filter(!is.na(all.birds)) %>% 
+  filter(!all.birds == "NULL")
+
+df.all.birds.names <-merge(df.all.birds.spp, alpha.names, by.x="all.birds", by.y= "X4.LETTER.CODE", all.x=TRUE)
+
+
+
+#### merge to check dicrepancies in files
+
+
+
+#92 spp in the all birds big data cleaned file
+
+#merging
+names(df.names1)
+
+df.names1$Plots_1991_Distri <- "Plots_1991_Distri"
+
+df.all.birds.merged <-merge(df.all.birds.names, df.names1, by.x = "all.birds", by.y ="x", all.x = TRUE)
+
+df.all.birds.merged <- df.all.birds.merged %>% 
+  select(!c("SCIENTIFIC.NAME", "X6.LETTER.CODE"))
+
+
+df.names2$'1991_Present_Mean' <-"1991_Present_Mean"
+
+df.all.birds.merged <-merge(df.all.birds.merged, df.names2, by.x = "all.birds", by.y ="bird.names2", all.x = TRUE)
+
+df.names3  <- as.data.frame(x=bird.names3_cl)
+
+df.names3$'1991_Future_Mean' <- "1991_Future_Mean"
+df.all.birds.merged <-merge(df.all.birds.merged, df.names3, by.x = "all.birds", by.y ="bird.names3_cl", all.x = TRUE)
+
+
+df.names4$'Plots_1991_Refugia_Mean' <- "Plots_1991_Refugia_Mean"
+df.all.birds.merged <-merge(df.all.birds.merged, df.names4, by.x = "all.birds", by.y ="bird.names4", all.x = TRUE)
+
+
+
+
+
+df.names5$Ref_x_Hab_Suit_Mean <- "Ref_x_Hab_Suit_Mean"
+df.all.birds.merged <-merge(df.all.birds.merged, df.names5, by.x = "all.birds", by.y ="bird.names5", all.x = TRUE)
+
+df.bird.names6
+
+df.bird.names6$Ref_x_Hab_Suit_UPR <- "Ref_x_Hab_Suit_UPR"
+
+df.all.birds.merged <-merge(df.all.birds.merged, df.bird.names6, by.x = "all.birds", by.y ="x", all.x = TRUE)
+
+
+
+df.bird.names7$Ref_x_Hab_Suit_LWR <- "Ref_x_Hab_Suit_LWR"
+df.all.birds.merged <-merge(df.all.birds.merged, df.bird.names7, by.x = "all.birds", by.y ="x", all.x = TRUE)
+
+df.names8$BRT_1991 <- "BRT_1991"
+df.all.birds.merged <-merge(df.all.birds.merged, df.names8, by.x = "all.birds", by.y ="bird.names8", all.x = TRUE)
+
+
+
+df.names9$BRT_1961 <- "BRT_1961"
+
+df.all.birds.merged <-merge(df.all.birds.merged, df.names9, by.x = "all.birds", by.y ="bird.names9", all.x = TRUE)
+
+
+write.csv(df.all.birds.merged, "data/df.all.birds.merged.csv")
+
+################################################
 names(all.birds)
 unique(all.birds$spp) #there is a NULL and an NA row
 length(unique(all.birds$spp))
 
-all.birds[is.na(all.birds$spp),]
-all.birds<-all.birds[!is.na(all.birds$spp),]
+all.birds[is.na(all.birds$spp),] ##RANDOM NA observation row
+
+all.birds<-all.birds[!is.na(all.birds$spp),] #get rid of NA row
+
 unique(all.birds$spp)
+
 length(unique(all.birds$spp))
+
 
 all.birds[which(all.birds$spp == "NULL"),] #### NULL
 
-length(all.birds[which(all.birds$spp == "NULL"),]) ## 13 observations, do we care???
+nrow(all.birds[which(all.birds$spp == "NULL"),]) ## 69276 observations are NULL, do we care??? 
+#they all have COUnt ZERO? maybe this is it?
 
+all.birds %>% 
+  group_by(spp) %>% 
+  summarise(
+    min = min(count, rm.na = TRUE),
+    max = max(count, rm.na = TRUE),
+    aver = mean(count, rm.na = TRUE)
+  ) %>% 
+  View()
+
+
+
+bird.names2
+
+all.the.birds <- unique(all.birds$spp)
+
+not.modelled <- setdiff(all.the.birds, bird.names2)
+
+modelled <- tibble(all.the.birds) %>% 
+  filter(!all.the.birds %in% not.modelled) %>% pull(all.the.birds)
+
+
+## BEWARE to switch from modelled to not modelled vector, respectively
+## change name TOO IN GGSAVE!!!!!!!
+four_plots <- list()
+
+for (i in sort(modelled)) {
+  onespp <- all.birds %>% 
+    filter(spp == i)
+  
+  p <- ggplot(onespp)+
+    geom_histogram(aes(count))+
+    theme_bw()+
+    ggtitle(i)+
+    xlab(NULL)
+
+  four_plots[[i]] <- p
+}
+
+plot_chunks <- split(four_plots, rep(1:ceiling(length(four_plots)/20), each = 20, length.out = length(four_plots)))
+
+for (i in seq_along(plot_chunks)) {
+  
+  p<-cowplot::plot_grid(plotlist = plot_chunks[[i]], nrow = 4, ncol =5)
+  
+  #print(p)
+  
+  ggsave(paste0("plots/model_bird_plots_",i,".png"), p, units = 'in', width = 17, height = 9)
+}
+
+### VIS NOT WORKING WITH TRELLISCOPEJS
+all.birds %>% 
+  filter(!spp=="NULL") %>% 
+  filter(spp %in% not.modelled) %>% 
+ggplot(aes(count))+
+  geom_density()+
+  theme_bw()+
+  #ggtitle(unique(spp))+
+  xlab(NULL)+
+  facet_trelliscope(
+    ~spp,
+    ncol = 4,
+    nrow = 4
+  )
+###
+
+
+###################################################################
 bird.data.names <- sort(unique(all.birds$spp), decreasing = FALSE)
 bird.data.names
 
@@ -77,3 +458,43 @@ ggplot()+
       axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
     legend.position = "none"
   )
+
+############ understanding file types
+
+#Files_1991_Future_Mean90CI_rds
+
+test1 <- readRDS(file.choose()) #ALFLCNRMESM21__26_2071.rds
+class(test1) #"data.table" "data.frame"
+nrow(test1)
+#2116556
+
+head(test1)
+#X       Y   Mean Upper90_CI Lower90_CI
+#1 -2617500 3385500 0.0561     0.1226     0.0106
+#2 -2616500 3385500 0.0649     0.1140     0.0120
+#3 -2615500 3385500 0.0643     0.1299     0.0067
+#4 -2614500 3385500 0.0632     0.1190     0.0125
+#5 -2613500 3385500 0.0542     0.1169     0.0047
+#6 -2612500 3385500 0.0430     0.1120     0.0019
+
+#Files_1991_Present_Mean90CI_rds
+test1 <- readRDS(file.choose()) #ALFL1991NormPresent.rds
+class(test1) #"data.frame"
+nrow(test1)
+#3605095
+
+head(test1)
+# X       Y   Mean  UPR90 LWR90
+#3335 -2249500 3784500 0.0182 0.0517 0e+00
+#3336 -2248500 3784500 0.0167 0.0503 0e+00
+#3337 -2247500 3784500 0.0403 0.1136 5e-04
+#5727 -2250500 3783500 0.0159 0.0441 0e+00
+#5728 -2249500 3783500 0.0179 0.0481 0e+00
+#5729 -2248500 3783500 0.0157 0.0441 0e+00
+
+#Plots_1991_Normal_Present Distributions
+
+#Plots_1991_Normal_Refugia --> Refugia, future suit, suit x refugia
+
+#Rasters_1991_Normal_Refugia and Habitat Suitability - tif files, individually for Refugia, Suit, and SuitxRef
+
